ec92db671f93ed164e2b839580633c2ceecff7ab
Z-With-Glasses <zgreen777@gmail.com>
2022-09-30 06:08:08 -0700

    Debugging and Printing Control
    Added std::cin input for debugging control and output for arrays through the console. Debugging output can be enabled and it allows choosing which arrays to print.

--- UATS.cpp
+++ UATS.cpp
@@ -1,12 +1,12 @@
 #include <assert.h>
-#include <chrono>//comment out if not testing run speed
+#include <chrono>
 #include <iostream>
 #include <random>
 #include <stdlib.h>
 #include <time.h>
 
 //DO NOT SET numberOfItems ABOVE 300 IF YOUR COMPILER'S LONG DOUBLE RESOLVES TO DOUBLE
-const int typesOfItems{ 5 };//(X)number of types of items in each NFT (eg if 5 then 5 unique items boots/chest/pants/gloves/hats
+const int typesOfItems{ 5 };//(X)number of types of items in each NFT (eg if 5 then 5 unique items boots/chest/pants/gloves/hats(above 150 results in INF)
 const int numberOfItems{ 300 };//(N)number of items for each type of item (eg if 10 then 10 item boots/hats/pants etc.) !!!!INF IF ABOVE 300, long double limit for some compilers is 1e+309!!!!
 const int amountOfNFTs{ typesOfItems * numberOfItems };//amount of NFTs to be created, tOI * nOI is not the max but it is safe
 static double arrayOfItems[typesOfItems][numberOfItems]{};//X[array#]Y[boots,pants,etc.]
@@ -14,6 +14,11 @@ static double NFT[typesOfItems]{};
 static double arrayOfNFTs[amountOfNFTs][typesOfItems]{};//X[array#]Y[boots,pants,etc.]
 static double NFTsums[amountOfNFTs]{};
 static int amountOfNFTsCreated{ 0 };
+bool FAOIdebugging{};
+bool BNFTdebugging{};
+bool FAONFTdebugging{};
+bool UGCdebugging{};
+
 
 std::mt19937 gen((unsigned)time(0));
 
@@ -32,8 +37,10 @@ void fillArrayOfItems()
 		for (int innerIt{ 0 }; innerIt < numberOfItems; innerIt++)
 		{
 			arrayOfItems[outerIt][innerIt] = value;//set element to array
-//std::cout << "ITEMARRAY assigning " << value << " to array " << outerIt << " at index " << innerIt << '\n';
+
 			value = value * 10;//set previous value to new value
+			if (FAOIdebugging)
+				std::cout << "ITEMARRAY assigning " << value << " to array " << outerIt << " at index " << innerIt << '\n';
 		}
 		valueKey = valueKey * 2;// double key value for next array
 	}
@@ -45,7 +52,8 @@ void buildNFT()
 	{
 		int randomItem{ random(0, numberOfItems - 1) };//-1 so the RNG generates from 0 to the size of the array - 1, since the array starts at 0 instead of 1
 		NFT[it] = arrayOfItems[it][randomItem];
-//std::cout << "buildNFT() assigning " << arrayOfItems[it][randomItem] << " to index " << it  << " RNG= " << randomItem << '\n';
+		if (BNFTdebugging)
+			std::cout << "buildNFT() assigning " << arrayOfItems[it][randomItem] << " to index " << it << " RNG= " << randomItem << '\n';
 	}
 }
 
@@ -88,12 +96,14 @@ void fillArrayOfNFTs()
 				for (int innerIt{ 0 }; innerIt < typesOfItems; innerIt++)//store new NFT
 				{
 					arrayOfNFTs[outerIt][innerIt] = NFT[innerIt];
-//std::cout << "NFTARRAYARRAY assigning " << NFT[innerIt] << " to array " << outerIt << " at index " << innerIt << '\n';
+					if (FAONFTdebugging)
+						std::cout << "NFTARRAYARRAY assigning " << NFT[innerIt] << " to array " << outerIt << " at index " << innerIt << '\n';
 				}
 			}
 			else {
 				duplicateNFT_Created = true;
-//std::cout << "NFT# "<< outerIt + 1 <<" duplicateNFT created, trying again" << '\n';//+1 so that the first created is NFT #1
+				if (FAONFTdebugging)
+					std::cout << "NFT# " << outerIt + 1 << " duplicateNFT created, trying again" << '\n';//+1 so that the first created is NFT #1
 			}
 		}
 	}
@@ -103,7 +113,8 @@ void uniqueGuaranteeCheck()
 	int totalNonUniqueNFTs{};
 	for (int it{ 0 }; it < amountOfNFTs - 1; it++)
 	{
-//std::cout << "Comparing sum for NFT #" << it + 1 << '-' << NFTsums[it] << " against NFT#" << it + 2 << '-' << NFTsums[it+1] << '\n';
+		if (UGCdebugging)
+			std::cout << "Comparing sum for NFT #" << it + 1 << '-' << NFTsums[it] << " against NFT#" << it + 2 << '-' << NFTsums[it + 1] << '\n';
 		if (NFTsums[it] == NFTsums[it + 1])//iterate through stored and sorted sums, break if duplicate found
 		{
 			std::cout << "NFT SUMS INDEX#" << it << '=' << NFTsums[it] << " which is the same as " << "NFT SUMS INDEX#" << it + 1 << '=' << NFTsums[it + 1] << '\n';
@@ -147,38 +158,74 @@ void printNFT_Sums()
 	}
 	std::cout << '\n';
 }
+int input()
+{
+	char input{};
+	bool cont{ 1 };
+	while (cont)
+	{
+		std::cin >> input;
+		if (input == 'Y' || input == 'y')
+		{
+			cont = false;
+			return true;
+		}
+		else if (input == 'N' || input == 'n')
+		{
+			cont = false;
+			return false;
+		}
+		std::cout << "Please enter Y for yes or N for no." << '\n';
+	}
+}
 int main()
 {
+	std::cout << "Type Y for Yes or N for No." << '\n';
+	std::cout << "Run with fillArrayOfItems() debugging?" << '\n';
+	if (input())
+		FAOIdebugging = true;
+	std::cout << "Run with buildNFT() debugging?" << '\n';
+	if (input())
+		BNFTdebugging = true;
+	std::cout << "Run with fillArrayOfNFTs() debugging?" << '\n';
+	if (input())
+		FAONFTdebugging = true;
+	std::cout << "Run with uniqueGuaranteeCheck() debugging?" << '\n';
+	if (input())
+		UGCdebugging = true;
+
 	std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
 	fillArrayOfItems();
 	fillArrayOfNFTs();
 	std::sort(NFTsums, NFTsums + amountOfNFTs);
-
-	//printItemArrays();
-	//printNFTs();
-	//printNFT_Sums();
-
-	uniqueGuaranteeCheck();
-
 	std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
 	std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
 
+	std::cout << "Print Item Arrays?" << '\n';
+	if (input())
+		printItemArrays();
+	std::cout << "Print NFTs?" << '\n';
+	if (input())
+		printNFTs();
+	std::cout << "Print NFT sums?" << '\n';
+	if (input())
+		printNFT_Sums();
+
+	uniqueGuaranteeCheck();
 	std::cout << "Amount of NFTs created = " << amountOfNFTsCreated << '\n' << "Run Time: " << time_span.count() << '\n';
+
+	//reset globals
+	arrayOfItems[typesOfItems - 1][numberOfItems - 1] = 0;
+	NFT[typesOfItems - 1] = 0;
+	arrayOfNFTs[amountOfNFTs - 1][typesOfItems - 1] = 0;
+	NFTsums[amountOfNFTs - 1] = 0;
+	amountOfNFTsCreated = 0;
+	FAOIdebugging = false;
+	BNFTdebugging = false;
+	FAONFTdebugging = false;
+	UGCdebugging = false;
+
 	std::cout << "Run again? Y/N" << '\n';
-	
-	bool cont{1};
-	while (cont)
-	{
-		char input{};
-		std::cin >> input;
-		if (input == 'Y' || input == 'y')
-		{
-			main();
-		}
-		else if (input == 'N' || input == 'n')
-		{
-			return 0;
-		}
-		std::cout << "Enter Y to continue or N to exit" << '\n';
-	}
+	if (input())
+		main();
 }
